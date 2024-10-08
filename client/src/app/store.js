b251e820956573f07f533364ec89f8b664313179
Lin Zhu <zhulin1362338@gmail.com>
2024-10-08 14:25:33 -0700

    add user slice

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import registrationReducer from '../features/registrationSlice';
+import userReducer from '../features/userSlice';
 
 const store = configureStore({
   reducer: {
     registrationSlice: registrationReducer,
+    userSlice: userReducer,
   },
 });
 
