7f0c754ae2d0819bff8e60dc9ede3d7922a81376
Lin Zhu <zhulin1362338@gmail.com>
2024-10-08 14:26:14 -0700

    update auth api

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -3,13 +3,13 @@ import api from './base';
 const API_URL = '/auth';
 
 export const login = async ({ userName, password }) => {
-  const response = await api.post(`${API_URL}/lgoin`, { userName, password });
+  const response = await api.post(`${API_URL}/login`, { userName, password });
 
   return response.data;
 };
 
-export const signup = async ({ userName, password }) => {
-  const response = await api.post(`${API_URL}/resgister`, {
+export const signup = async ({ userName, password, registerToken }) => {
+  const response = await api.post(`${API_URL}/register/${registerToken}`, {
     userName,
     password,
   });
