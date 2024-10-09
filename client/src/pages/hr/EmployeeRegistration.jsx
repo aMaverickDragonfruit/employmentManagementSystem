ec8e6cfc1c30799ae2feb37d7de5809e89818e1e
XiaoweiweiZheng <xiaoweiweizheng@gmail.com>
2024-10-08 19:16:19 -0700

    adding onboarding form fields

--- client/src/pages/hr/EmployeeRegistration.jsx
+++ client/src/pages/hr/EmployeeRegistration.jsx
@@ -64,17 +64,37 @@ const InvitationForm = ({ onSubmit, onClose, err }) => {
   );
 };
 
-const RegistrationTable = ({ data }) => {
+const RegistrationTable = ({ setErr, data }) => {
+  const dispatch = useDispatch();
+
+  const handleSendReminder = (record) => {
+    const data = {
+      email: record.email,
+    };
+    dispatch(createRegistration(data)).then((action) => {
+      if (createRegistration.fulfilled.match(action)) {
+        dispatch(fetchRegistrations());
+      } else if (createRegistration.rejected.match(action)) {
+        setErr(action.payload);
+      }
+    });
+  };
   const columns = [
     {
       title: 'Last Updated',
       dataIndex: 'updatedAt',
       key: 'updatedAt',
+      render: (text) => {
+        const date = new Date(text);
+        return date.toLocaleString();
+      },
+      sorter: (a, b) => new Date(a.updatedAt) - new Date(b.updatedAt),
     },
     {
       title: 'Name',
       dataIndex: 'name',
       key: 'name',
+      sorter: (a, b) => a.name.localeCompare(b.name),
     },
     {
       title: 'Email',
@@ -85,32 +105,68 @@ const RegistrationTable = ({ data }) => {
       title: 'Registration Link',
       dataIndex: 'registrationLink',
       key: 'registrationLink',
+      render: (_, record) => (
+        <a
+          href={record.registrationLink}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Registration Link
+        </a>
+      ),
     },
     {
       title: 'Status',
       dataIndex: 'status',
       key: 'status',
+      sorter: (a, b) => a.name.localeCompare(b.name),
+    },
+    {
+      title: 'Action',
+      dataIndex: 'action',
+      key: 'action',
+      render: (action, record) => {
+        if (action === 'Send Reminder') {
+          return <a onClick={() => handleSendReminder(record)}>{action}</a>;
+        } else if (action) {
+          return <span>{action}</span>;
+        } else {
+          return null;
+        }
+      },
+      sorter: (a, b) => a.name.localeCompare(b.name),
     },
   ];
 
-  // const dataSource = data.map();
-  const dataSource = [];
+  // const dataSource = [];
+  const dataSource = data.map((element) => {
+    return {
+      key: element._id,
+      updatedAt: element.updatedAt,
+      name: element.name,
+      email: element.email,
+      registrationLink: element.registrationLink,
+      status: element.status ? 'Accepted' : 'Pending',
+      action: element.status ? 'N/A' : 'Send Reminder',
+    };
+  });
 
   return (
     <Table
-      dataSource={data}
+      dataSource={dataSource}
       columns={columns}
     />
   );
 };
 
 export default function EmployeeRegistration() {
+  const [err, setErr] = useState(null);
+
   const dispatch = useDispatch();
   const { registrations, loading, error } = useSelector(
     (state) => state.registrationSlice
   );
   const [showInvitation, setShowInvitation] = useState(false);
-  const [err, setErr] = useState(null);
 
   useEffect(() => {
     dispatch(fetchRegistrations());
@@ -119,7 +175,13 @@ export default function EmployeeRegistration() {
   console.log(registrations);
 
   const onSubmit = (data) => {
-    dispatch(createRegistration(data));
+    dispatch(createRegistration(data)).then((action) => {
+      if (createRegistration.fulfilled.match(action)) {
+        dispatch(fetchRegistrations());
+      } else if (createRegistration.rejected.match(action)) {
+        setErr(action.payload);
+      }
+    });
   };
 
   const onClose = () => {
@@ -142,7 +204,10 @@ export default function EmployeeRegistration() {
           onClose={onClose}
         />
       )}
-      <RegistrationTable data={registrations} />
+      <RegistrationTable
+        setErr={setErr}
+        data={registrations}
+      />
     </>
   );
 }
