088d746d078f86b75a7950507d1c05edb83c3a54
XiaoweiweiZheng <xiaoweiweizheng@gmail.com>
2024-10-09 20:42:05 -0700

    impletement employee profiles table header

--- client/src/pages/hr/EmployeeProfiles.jsx
+++ client/src/pages/hr/EmployeeProfiles.jsx
@@ -1,3 +1,59 @@
+import { Table, Typography } from 'antd';
+import PageLayout from '../../components/layout/Page';
+const { Title } = Typography;
+
+const ProfilesTable = (data) => {
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: (_, record) => {
+        <Typography.Link>{record.name}</Typography.Link>;
+      },
+      sorter: (a, b) => a.name.localeCompare(b.name),
+    },
+    {
+      title: 'SSN',
+      dataIndex: 'ssn',
+      key: 'ssn',
+      sorter: (a, b) => a.name.localeCompare(b.name),
+    },
+    {
+      title: 'Work Authorization',
+      dataIndex: 'workAuthorization',
+      key: 'workAuthorization',
+      sorter: (a, b) => a.name.localeCompare(b.name),
+    },
+    {
+      title: 'Phone',
+      dataIndex: 'phone',
+      key: 'phone',
+      sorter: (a, b) => a.name.localeCompare(b.name),
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+      sorter: (a, b) => a.name.localeCompare(b.name),
+    },
+  ];
+
+  const dataSource = [];
+
+  return (
+    <Table
+      dataSource={dataSource}
+      columns={columns}
+    />
+  );
+};
+
 export default function EmployeeProfiles() {
-  return <h1>Employee Profiles</h1>;
+  return (
+    <PageLayout>
+      <Title>Employee Profiles</Title>
+      <ProfilesTable />
+    </PageLayout>
+  );
 }
