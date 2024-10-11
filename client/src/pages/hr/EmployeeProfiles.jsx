dea68d2aae12c2106368c5091025f8e3f308dcc1
XiaoweiweiZheng <xiaoweiweizheng@gmail.com>
2024-10-10 18:53:14 -0700

    implement employee profiles and employee visa status page

--- client/src/pages/hr/EmployeeProfiles.jsx
+++ client/src/pages/hr/EmployeeProfiles.jsx
@@ -1,16 +1,18 @@
 import { Table, Typography } from 'antd';
 import PageLayout from '../../components/layout/Page';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProfiles } from '../../features/profileSlice';
+import { useEffect } from 'react';
 const { Title } = Typography;
 
-const ProfilesTable = (data) => {
+const ProfilesTable = ({ data }) => {
   const columns = [
     {
       title: 'Name',
       dataIndex: 'name',
       key: 'name',
-      render: (_, record) => {
-        <Typography.Link>{record.name}</Typography.Link>;
-      },
+      //to be completed
+      render: (_, record) => <Typography.Link>{record.name}</Typography.Link>,
       sorter: (a, b) => a.name.localeCompare(b.name),
     },
     {
@@ -39,7 +41,16 @@ const ProfilesTable = (data) => {
     },
   ];
 
-  const dataSource = [];
+  const dataSource = data.map((element) => {
+    return {
+      key: element._id,
+      name: element.lastName,
+      ssn: element.ssn || 'None',
+      workAuthorization: element.workAuthType,
+      phone: element.cellPhone || 'None',
+      email: element.email,
+    };
+  });
 
   return (
     <Table
@@ -50,10 +61,21 @@ const ProfilesTable = (data) => {
 };
 
 export default function EmployeeProfiles() {
+  const dispatch = useDispatch();
+  const { profiles, curProfile, loading, error } = useSelector(
+    (state) => state.profileSlice
+  );
+
+  console.log(profiles);
+
+  useEffect(() => {
+    dispatch(fetchProfiles());
+  }, [dispatch]);
+
   return (
     <PageLayout>
       <Title>Employee Profiles</Title>
-      <ProfilesTable />
+      <ProfilesTable data={profiles} />
     </PageLayout>
   );
 }
--- client/src/pages/hr/EmployeeVisaStatus.jsx
+++ client/src/pages/hr/EmployeeVisaStatus.jsx
@@ -1,11 +1,161 @@
-import { Typography } from 'antd';
+import { Table, Typography, Tag } from 'antd';
 import PageLayout from '../../components/layout/Page';
 const { Title } = Typography;
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProfiles } from '../../features/profileSlice';
+import { useEffect } from 'react';
+import dayjs from 'dayjs';
+
+const EmployeeVisaStatusTable = ({ data }) => {
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      //to be completed profiles/:id link
+      render: (_, record) => <Typography.Link>{record.name}</Typography.Link>,
+      sorter: (a, b) => a.name.localeCompare(b.name),
+    },
+    {
+      title: 'Work Visa Type',
+      dataIndex: 'visaType',
+      key: 'visaType',
+      sorter: (a, b) => a.name.localeCompare(b.name),
+    },
+    {
+      title: 'Work Authorization Start Date',
+      dataIndex: 'startDate',
+      key: 'startDate',
+      sorter: (a, b) => a.name.localeCompare(b.name),
+      render: (text) => {
+        const date = new Date(text);
+        return date.toLocaleDateString();
+      },
+    },
+    {
+      title: 'Work Authorization End Date',
+      dataIndex: 'endDate',
+      key: 'endDate',
+      sorter: (a, b) => a.name.localeCompare(b.name),
+      render: (text) => {
+        const date = new Date(text);
+        return date.toLocaleDateString();
+      },
+    },
+    {
+      title: 'Next Step',
+      dataIndex: 'nextStep',
+      key: 'nextStep',
+      sorter: (a, b) => a.name.localeCompare(b.name),
+      render: (_, { status }) => {
+        let color = 'red';
+        let label = 'Waiting for submission';
+        if (status === 'Pending') {
+          color = 'green';
+          label = 'Waiting for Approval';
+        } else if (status === 'Approved') {
+          color = 'blue';
+          label = 'No action needed';
+        }
+        return (
+          <Tag
+            color={color}
+            key={status}
+          >
+            {label.toUpperCase()}
+          </Tag>
+        );
+      },
+    },
+    {
+      title: 'Action',
+      dataIdex: 'action',
+      key: 'action',
+      sorter: (a, b) => a.name.localeCompare(b.name),
+      render: (_, { status, userId }) => {
+        let label = 'Send Notification';
+        if (status === 'Pending' || status === 'Approved')
+          label = 'View Document';
+        // wait for complete the link
+        return (
+          <Typography.Link
+            href={`http://localhost:3001/profiles/users/${userId}`}
+          >
+            {label}
+          </Typography.Link>
+        );
+      },
+    },
+  ];
+
+  const dataSource = data.map((element) => {
+    let status = 'Approval';
+    // if no document
+    if (element.documents.length === 0) {
+      status = 'Reject';
+    } else if (
+      element.documents.length === 1 &&
+      element.documents[0].fileName === 'ProfilePicture'
+    ) {
+      status = 'Reject';
+    } else {
+      for (let i = 0; i < element.documents.length; i++) {
+        if (element.documents[i].status === 'Reject') {
+          status = 'Reject';
+        } else if (element.documents[i].status === 'Pending') {
+          status = 'Pending';
+        } else {
+          continue;
+        }
+      }
+    }
+
+    // if (!element.documents[0]) {
+    //   status = 'Reject';
+    // } else {
+    // element.documents.slice(1).forEach((element) => {
+    //   console.log(element.status);
+    //   if (element.status === 'Rejected') {
+    //     status = 'Reject';
+    //   }
+    // });
+    // },
+
+    return {
+      key: element._id,
+      userId: element.user,
+      name: element.firstName + ' ' + element.lastName,
+      visaType: element.workAuthType,
+      startDate: element.workAuthStartDate || 'None',
+      endDate: element.workAuthEndDate || 'None',
+      status: status,
+    };
+  });
+
+  return (
+    <Table
+      columns={columns}
+      dataSource={dataSource}
+    ></Table>
+  );
+};
 
 export default function EmployeeVisaStatus() {
+  const dispatch = useDispatch();
+  const { profiles, curProfile, loading, error } = useSelector(
+    (state) => state.profileSlice
+  );
+
+  console.log(profiles);
+
+  useEffect(() => {
+    dispatch(fetchProfiles());
+  }, [dispatch]);
+
   return (
     <PageLayout>
       <Title>Employee Visa Status</Title>
+      <EmployeeVisaStatusTable data={profiles} />
     </PageLayout>
   );
 }
