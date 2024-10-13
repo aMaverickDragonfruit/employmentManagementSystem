3a7bcf0812cf458d5a1305d49baf2e2ba438297d
Lin Zhu <zhulin1362338@gmail.com>
2024-10-13 16:38:47 -0700

    Added mailRouter to index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import userRouter from './routers/users.js';
 import authRouter from './routers/auth.js';
 import profileRouter from './routers/profiles.js';
 import registrationRouter from './routers/registration.js';
+import mailRouter from './routers/mailer.js';
 
 const app = express();
 const port = 3000;
@@ -20,13 +21,13 @@ app.use('/api/users', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/profiles', profileRouter);
 app.use('/api/registrations', registrationRouter);
+app.use('/api', mailRouter);
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.statusCode = 404;
   next(err);
 });
-
 
 app.use(errorHandlerMiddleware);
 
