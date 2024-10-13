d006dbfe032cfbfedd4d62723105bbec66759868
XiaoweiweiZheng <xiaoweiweizheng@gmail.com>
2024-10-13 16:44:29 -0700

    implemented next step for visa management

--- client/src/pages/employee/VisaManagementComponents.jsx
+++ client/src/pages/employee/VisaManagementComponents.jsx
@@ -1,9 +1,21 @@
 import { DownloadOutlined } from '@ant-design/icons';
-import { Alert, Upload, Button, Steps } from 'antd';
+import { Alert, Upload, Button, Steps, Typography, Form, message } from 'antd';
+import { useState } from 'react';
+const { Title } = Typography;
 
-export const Message = ({ curStep, curStatus, feedback }) => {
+export const Message = ({ currentStep, curStatus, feedback }) => {
+  if (curStatus === 'Pending') {
+    return (
+      <Alert
+        showIcon
+        type='info'
+        message='HR is reviewing'
+      />
+    );
+  }
+
   let file = '';
-  switch (curStep) {
+  switch (currentStep) {
     case 1:
       file = 'EAD Card';
       break;
@@ -17,7 +29,7 @@ export const Message = ({ curStep, curStatus, feedback }) => {
       file = '';
   }
 
-  const type = curStatus === 'Rejected' ? 'error' : 'info';
+  const type = curStatus === 'Rejected' ? 'error' : 'warning';
   const description = curStatus === 'Rejected' ? feedback : '';
 
   return (
@@ -28,32 +40,6 @@ export const Message = ({ curStep, curStatus, feedback }) => {
       type={type}
       style={{ width: '40%' }}
     />
-  );
-};
-
-const uploadProps = {
-  name: 'file',
-  action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
-  headers: {
-    authorization: 'authorization-text',
-  },
-  onChange(info) {
-    if (info.file.status !== 'uploading') {
-      console.log(info.file, info.fileList);
-    }
-    if (info.file.status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully`);
-    } else if (info.file.status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-};
-
-export const FileUpload = () => {
-  return (
-    <Upload {...uploadProps}>
-      <Button>Click here to upload</Button>
-    </Upload>
   );
 };
 
@@ -73,46 +59,185 @@ export const VisaSteps = ({ visaDocuments, feedback }) => {
     },
   ];
 
-  let curStep = 1; // once user able to view this page opt receipt has been approved
-  let curStatus = '';
-  console.log(`init cur step: ${curStep}`);
+  // no visa documents uploaded yet, start uploading the EAD card
+  // if (visaDocuments.length === 1) {
+  //   return (
+  //     <>
+  //       <Alert
+  //         message='Please submit your EAD card'
+  //         showIcon
+  //         type='warning'
+  //       />
+  //       <FileUpload />
+  //       <Steps
+  //         items={stepItems}
+  //         current={1}
+  //         status='process'
+  //       />
+  //     </>
+  //   );
+  // }
 
-  // no visa documents uploaded
-  if (visaDocuments.length === 1) {
-    curStep = 1;
-  } else {
-    // any visa docs uploaded, iterate the list
-    for (let i = 1; i < visaDocuments.length; i++) {
-      // current step stop at not approved file
-      if (visaDocuments[i].status !== 'Approved') {
-        curStep = i;
-        curStatus = visaDocuments[i].status;
-        break;
-      }
-      curStep++;
+  // have visa docs uploaded, iterate the list, to check cur status and cur step
+  let curStep = 1; // review from the EAD
+  let curStatus = visaDocuments[1]?.status || '';
+  let curFileName = '';
+
+  for (let i = 1; i < visaDocuments.length; i++) {
+    // if cur doc has not been approved, cannot move on next step
+    if (visaDocuments[i].status !== 'Approved') {
+      curStep = i;
+      curStatus = visaDocuments[i].status;
+      curFileName = visaDocuments[i].fileName;
+      break;
     }
+    // move on next step, cur status is approved
+    curStep++;
+    curStatus = 'Approved';
   }
 
-  // if cur status is rejected step status set to error, else set to process
+  // all doc has been submitted and approved
+  if (curStep === 4 && curStatus === 'Approved') {
+    return (
+      <>
+        <Alert
+          message='All documents has been approved!'
+          type='success'
+          showIcon
+        />
+        <Steps
+          items={stepItems}
+          current={curStep}
+          status='finished'
+        />
+      </>
+    );
+  }
+
+  // uploaded visa docs, but have not been approved. return message according next step and cur status
   const status = curStatus === 'Rejected' ? 'error' : 'process';
-
-  console.log(`curStep: ${curStep}`);
-  console.log(`curStatus: ${curStep}`);
-
   return (
     <>
       <Message
-        curStep={curStep}
+        currentStep={curStep}
         curStatus={curStatus}
         feedback={feedback}
       />
-      {curStep < 4 && <FileUpload />}
+      {(curStatus === 'New' || curStatus === 'Rejected') && (
+        <FileUpload curFileName={curFileName} />
+      )}
+      {curStep === 2 && curStatus !== 'Pending' && <I983Templates />}
       <Steps
         items={stepItems}
         current={curStep}
         status={status}
       />
     </>
+  );
+};
+
+const UploadForm = ({ uploadFileName }) => {
+  const [form] = Form.useForm();
+
+  // for uploading file fields
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const normFile = (e) => {
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e && e.fileList;
+  };
+
+  const uploadProps = {
+    name: 'file',
+    action: 'http://localhost:6173/upload',
+    headers: {
+      authorization: 'authorization-text',
+    },
+    onChange(info) {
+      console.log(info);
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === 'done') {
+        messageApi.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === 'error') {
+        messageApi.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
+
+  const beforeFileUpload = (file) => {
+    // console.log(file);
+    // Optionally validate file type and size
+    const isPdf = file.type.startsWith('application/pdf');
+    if (!isPdf) {
+      messageApi.error('You can only upload pdf files!');
+      return Upload.LIST_IGNORE;
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      messageApi.error('Image must be smaller than 2MB!');
+      return Upload.LIST_IGNORE;
+    }
+
+    return true;
+  };
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Form
+      layout='vertical'
+      name='visa-doc-upload'
+      onFinish={onSubmit}
+      onSubmitCapture={(e) => e.preventDefault()}
+      style={{ border: 'solid', minWidth: '25%', padding: '20px' }}
+    >
+      {contextHolder}
+      <Form.Item
+        key='visa-doc'
+        name={uploadFileName}
+        label='Please upload your file'
+        valuePropName='fileList'
+        getValueFromEvent={normFile}
+      >
+        <Upload
+          {...uploadProps}
+          maxCount={1}
+          beforeUpload={beforeFileUpload}
+        >
+          <Button>Choose your file</Button>
+        </Upload>
+      </Form.Item>
+
+      <Button
+        type='primary'
+        size='small'
+        htmlType='submit'
+        style={{ marginTop: '40px' }}
+      >
+        Upload
+      </Button>
+    </Form>
+  );
+};
+
+export const FileUpload = ({ curFileName }) => {
+  const [isUploadOpen, setIsUploadOpen] = useState(false);
+
+  return (
+    <div className='w-1/3'>
+      {!isUploadOpen && (
+        <Button onClick={() => setIsUploadOpen(true)}>
+          Click here to upload
+        </Button>
+      )}
+      {isUploadOpen && <UploadForm uploadFileName={curFileName} />}{' '}
+    </div>
   );
 };
 
@@ -145,18 +270,14 @@ const I983Props = {
     showRemoveIcon: false,
   },
   defaultFileList: i983FileList,
-  previewFile(file) {
-    console.log('Your upload file:', file);
-    // Your process logic. Here we just mock to the same file
-    return fetch('https://next.json-generator.com/api/json/get/4ytyBoLK8', {
-      method: 'POST',
-      body: file,
-    })
-      .then((res) => res.json())
-      .then(({ thumbnail }) => thumbnail);
-  },
 };
 
 export const I983Templates = () => {
-  return <Upload {...I983Props}></Upload>;
+  return (
+    <>
+      <Upload {...I983Props}>
+        <Title level={4}>I-983 Samples</Title>
+      </Upload>
+    </>
+  );
 };
