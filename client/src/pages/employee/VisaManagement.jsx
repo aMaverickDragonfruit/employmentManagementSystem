943209ab8dbb91d855a9c0cfd18831869cee66f9
Lin Zhu <zhulin1362338@gmail.com>
2024-10-13 21:47:55 -0700

    Implemented visa status page for empolyee

--- client/src/pages/employee/VisaManagement.jsx
+++ client/src/pages/employee/VisaManagement.jsx
@@ -1,26 +1,19 @@
 import { Typography } from 'antd';
 import PageLayout from '../../components/layout/Page';
-import { VisaSteps, I983Templates } from './VisaManagementComponents';
+import { VisaSteps } from './VisaManagementComponents';
+import { useSelector } from 'react-redux';
 const { Title } = Typography;
 
-const testingDoc = [
-  { fileName: 'OPT-receipt', status: 'New' },
-  { fileName: 'EAD', status: 'Approved' },
-  { fileName: 'i983', status: 'Rejected' },
-  { fileName: 'i20', status: 'Rejected' },
-];
-
-const feedback = 'this is a feedback from hr';
-
 export default function VisaManagement() {
+  const { curProfile } = useSelector((state) => state.profileSlice);
+  const userDocuments = curProfile.documents.slice(2);
+  // console.log(userDocuments);
+
   return (
     <PageLayout>
       <div className='flex flex-col gap-10'>
         <Title>Visa Management</Title>
-        <VisaSteps
-          visaDocuments={testingDoc}
-          feedback={feedback}
-        />
+        <VisaSteps visaDocuments={userDocuments} />
       </div>
     </PageLayout>
   );
