f5277d0997c9df2b017332ea08fdd1f4853ad403
XiaoweiweiZheng <xiaoweiweizheng@gmail.com>
2024-10-14 11:46:06 -0700

    Implemented home style

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,16 @@
+import { Typography } from 'antd';
+import PageLayout from '../components/layout/Page';
+const { Title } = Typography;
+
 export default function Home() {
-  return <div>HOME</div>;
+  return (
+    <PageLayout>
+      <Title>Welcome to Chuwa Management</Title>
+      <img
+        src='/asset/kun.jpg'
+        alt=''
+        style={{ width: '400px', height: '400px' }}
+      />
+    </PageLayout>
+  );
 }
