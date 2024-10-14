adafd913c253de49ca6b007f5c97676abf1de87f
Lin Zhu <zhulin1362338@gmail.com>
2024-10-14 11:46:21 -0700

    Redirect user to onboaring page if user application is not apporved

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,27 @@
+import { useSelector } from 'react-redux';
+import { Navigate, useLocation } from 'react-router-dom';
+import Page500 from '../pages/Page500';
+
 export default function Home() {
-  return <div>HOME</div>;
+  const location = useLocation();
+
+  const { curProfile, loading, error } = useSelector(
+    (state) => state.profileSlice
+  );
+
+  if (error) {
+    return <Page500 message={error} />;
+  }
+
+  //   // Handle loading state
+  if (loading || !curProfile) {
+    // Data is still loading
+    return null;
+  } else if (curProfile.status === 'Approved') {
+    return <div>HOME</div>;
+  } else {
+    return (
+      <Navigate to='/onboarding-form' state={{ from: location }} replace />
+    );
+  }
 }
